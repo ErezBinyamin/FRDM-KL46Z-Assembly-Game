7cae8b4c2c60c98a72647d21a3f108e4e1e340f5
Erez Binyamin <exb4096@rit.edu>
2017-05-10 07:44:09 -0400

    First working version
    Basic functional version of game

--- MainRun.c
+++ MainRun.c
@@ -17,41 +17,42 @@
 //Game Dynamics
 #define NUM_QUESTIONS (16)
 #define NUM_LEVELS (7)
-#define NUM_LEVEL_PARTS (3)
+#define NUM_LEVEL_PARTS (2)
 
 /* static constatnts */
 
-const static char rules1[] = "Welcome to the Ultimate Assembler!! \r\n \0";
-const static char foo[] = "In this game you will be given the Register Transfer Notaiton, RTN, for an ARM assembly instruction. \r\n \0";
-const static char foo1[] = "You will then have to type the corresponding ARM assembly instruction and press ENTER \r\n \0";
+const static char rules1[] = "Welcome to the Ultimate Assembler!! \r\n\0";
+const static char foo[] = "In this game you will be given the Register Transfer Notaiton, RTN, for an ARM assembly instruction \r\n\0";
+const static char foo1[] = "You will then have to type the corresponding ARM assembly instruction and press ENTER \r\n\0";
 const static char rules2[] = "Be careful though... \r\n \0";
-const static char foo2[] = "If you get the answer wrong you will lose a life! And there will be a timer running! \r\n \0";
-const static char rules3[] = "In short... \r\n *You'll get some RTN \r\n *You must enter the correct ARM instruction \r\n *You have 5 lives \r\n *Each level the time per question decreaces \r\n \0";
+const static char foo2[] = "There will be a timer running! If you run out of time you Loose a life!\r\n\0";
+const static char rules3[] = "In short... \r\n *You'll get some RTN \r\n *You must enter the correct ARM instruction \r\n *You have 3 lives \r\n *Each level the time per question decreaces \r\n\0";
 
 const static char *questions[NUM_QUESTIONS] = {"Ra <- ZeroExt(Imm8) \0", "Ra <- ~Rb \0", "Rj <- RSpecial \0", "RSpecial <- Rj \0", "Rm - Rn \0",
 "Ra + Rb \0", "Ra <- 0 - Rb \0", "Rx <- Rx + Ry \0", "Ra <- Rb + Rc \0", "Ra <- Ra + Rb + C \0", "SP <- SP + ZeroExt(Imm9) \0", "Ra <- Rb - Rc \0",
 "Ra <- Ra - Rb - C \0", "Ra <- Ra & Rb \0", "Ra <- Ra & ~Rb \0", "Ra <- Ra | Rb \0"};
 
-const static char *answers[NUM_QUESTIONS] = {"MOVS \0", "MVNS \0", "MRS \0", "MSR \0", "CMP \0", "CMN \0", "RSBS \0", 
-"ADD \0", "ADDS \0", "ADCS \0", "SUB \0", "SUBS \0", "SBCS \0", "ANDS \0", "BICS \0", "ORRS \0"};
+const static char *answers[NUM_QUESTIONS] = {"MOVS\0", "MVNS\0", "MRS\0", "MSR\0", "CMP\0", "CMN\0", "RSBS\0", 
+"ADD\0", "ADDS\0", "ADCS\0", "SUB\0", "SUBS\0", "SBCS\0", "ANDS\0", "BICS\0", "ORRS\0"};
 
 //Time in seconds alloted for each level
-const static int times[] = {60, 40, 20, 10, 5, 1, 0};
+const static int times[] = {30, 20, 15, 10, 5, 1, 0};
 
 //Rank achieved for finishing at a given level
 const static char *ranks[] = {"Retake DSD1 \0", "TA skillz \0", "Richard Tolleson \0", "Shanchieh Jay Yang \0", "Roy Melton \0",
 "Derek Freeman \0", "Erez Binyamin \0"};
 
 const static char newLine[] = "\r\n\0";
-const static char wrong[] = "Incorrect! You lose a life!\0";
+const static char wrong[] = "Incorrect!\0";
 const static char right[] = "Correct!\0";
 const static char outOfTimeError[] = "Out of Time! You lose a life!\0";
 const static char levelPrompt[] = "Good luck! You're on level :\0";
 const static char gameOver1[] = "Game Over! You made it to level : \0";
 const static char gameOver2[] = "And achieved a rank of : \0";
+const static char CurLife[] = "Life : \0";
 
 /* static variables */
-int life = 5;
+int life = 3;
 int level = 0;
 int part_Level = 0;
 int current = 0;
@@ -70,6 +71,7 @@ int same = FALSE;
 int outOfTime = FALSE;
 int i = 0;
 char newChar = '0';
+int done = FALSE;
 
 //Solved Array
 int solved[NUM_QUESTIONS] = {0};
@@ -87,7 +89,12 @@ void printRules()
 	PutStringSB ((char *)foo2, sizeof (rules3));
 }
 
-  
+void timeout()
+{
+	for(i=0;i<99999;i=i+1){}
+}
+
+extern int Count;
 int main (void) {   
   
   /* >>>>> Initialization Calls <<<<< */    
@@ -101,19 +108,29 @@ int main (void) {
   __asm("CPSIE   I");  /* unmask interrupts */
 
   /* >>>>> Remaining program code here <<<<< */
-	StartTime();
-    printRules();
-   do 
+	
+ printRules();
+ StartTime();
+   for(;;)
    {
+	   if(done == TRUE)
+	   {
+		   for(;;){}
+	   }
 	   PutStringSB ((char *)newLine, sizeof (newLine));
 	   PutStringSB ((char *)levelPrompt, sizeof (levelPrompt));
-	   PutChar (level + '0');
+	   PutChar (level + '0' + 1);
+	   PutStringSB ((char *)newLine, sizeof (newLine));
+	   
+	   PutStringSB ((char *)CurLife, sizeof (CurLife));
+	   PutChar (life + '0');
 	   PutStringSB ((char *)newLine, sizeof (newLine));
 	   
 	   //Get Next question
+	   loop = FALSE;
 	   while(loop == FALSE)
 	   {
-		   //Count = systemTime
+		   //RANDOM NUMBER
 		   current = Count % NUM_QUESTIONS;
 		   
 		   //.Contains() method
@@ -123,12 +140,13 @@ int main (void) {
 			   if(solved[i] == current)
 			   {
 				   temp = FALSE;
+				   break;
 			   }
 		   }
 		   
 		   if(temp == TRUE)
 		   {
-			   solved[place] = current;
+			   *(solved + place) = current;
 			   place = place + 1;
 			   loop = TRUE;
 		   }
@@ -137,43 +155,34 @@ int main (void) {
 	   //Set Count to 0;
 	   timeLimit = times[level];
 	   ClearTime();
-	   while(Count < timeLimit)
+	   
+	   //User input loop
+	   while(Count < timeLimit * 100)
 	   {
 		   PutStringSB ((char *)questions[current], MAX_STRING);
 		   PutStringSB ((char *)newLine, sizeof (newLine));
 		   
 		   //Get input
-		   for(i=0; i< MAX_STRING; i = i+1)
-		   {
-			   if(CheckRecQ() == 1)
-			   {
-				   newChar = GetChar();
-				   //CHECK for "ENTER" key
-				   if(newChar != 0x0D)
-				   {
-					   *(input + i) = newChar;
-				   }
-				   else{break;}
-			   }
-			   else
-			   {
-				   i = i-1;
-			   }
-		   }
+		   GetStringSB ((char *) input, MAX_STRING);
 		   PutStringSB ((char *)newLine, sizeof (newLine));
 		   
 		   //Check Answer
 		   same = SameStringSB((char *)answers[current], input);
+		   REDLED_OFF();
+		   GREENLED_OFF();
+		   
 		   
 		   //INCORRECT
 		   if(same == FALSE)
 		   {
+			   REDLED_ON();
 			   PutStringSB ((char *)wrong, sizeof (wrong));
 			   life = life - 1;
 		   }
 		   //CORRECT
 		   else
 		   {
+			   GREENLED_ON();
 			   PutStringSB ((char *)right, sizeof (right));
 			   same = TRUE;
 			   break;
@@ -185,8 +194,9 @@ int main (void) {
 	   if(same == FALSE)
 	   {
 		   PutStringSB ((char *)newLine, sizeof (newLine));
-		   
+		   //timeout();
 		   PutStringSB ((char *)outOfTimeError, sizeof (outOfTimeError));
+		   //timeout();
 		   life = life - 1;
 		   
 		   PutStringSB ((char *)newLine, sizeof (newLine));
@@ -198,18 +208,20 @@ int main (void) {
 		   part_Level = 0;
 		   level = level + 1;
 	   }
+	   
+	   if(life <= 0)
+	   {
+		   PutStringSB ((char *)newLine, sizeof (newLine));
+		   PutStringSB ((char *)gameOver1, sizeof (gameOver1));
+		   PutChar (level + '0' + 1);
+		   PutStringSB ((char *)newLine, sizeof (newLine));
+		  
+		   PutStringSB ((char *)gameOver2, sizeof (gameOver2));
+		   PutStringSB ((char *)ranks[level], MAX_STRING);
+		   PutStringSB ((char *)newLine, sizeof (newLine));
+		   done = TRUE;
+	   }
    }
-  while (life > 0 && level < NUM_LEVELS);
-  
-  StopTime();
-  
-  PutStringSB ((char *)gameOver1, sizeof (gameOver1));
-  PutChar (level + '0');
-  PutStringSB ((char *)newLine, sizeof (newLine));
-  
-  PutStringSB ((char *)gameOver2, sizeof (gameOver2));
-  PutStringSB ((char *)ranks[level], sizeof (ranks[level]));
-  PutStringSB ((char *)newLine, sizeof (newLine));
   
   return (0);
 } 
